68c5ce465001396ffb370b0ffdaca81ff5372c86
mtgrunt <mileswallace@lavascript.com>
2021-07-26 01:36:41 -0700

    added h1 and p

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome To My Website';
+  const delights = 50;
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Delighted { delights } times</p>
       </div>
     </div>
   );
