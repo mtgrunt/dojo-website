19298a41346689581ad0503d1cc70e037fef2545
mtgrunt <mileswallace@lavascript.com>
2021-08-02 02:16:41 -0700

    New BlogList

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -6,16 +7,11 @@ const Home = () => {
     { title: 'Welcome to the party!', body: 'lorem ipsum...', author: 'Cub', id: 2 },
     { title: 'California love!', body: 'lorem ipsum...', author: 'Guppy', id: 3 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Panda', id: 4 }
-  ])
+  ]);
 
   return (
     <div className="home">
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" />
     </div>
   );
 }
