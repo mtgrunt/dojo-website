c5192cbda567eef692344094f6b49ab1532af792
mtgrunt <mileswallace@lavascript.com>
2021-08-08 01:06:02 -0700

    Updated Home

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,16 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'I am intercontinental, cuz!', body: 'lorem ipsum...', author: 'Bear', id: 1 },
-    { title: 'Welcome to the party!', body: 'lorem ipsum...', author: 'Cub', id: 2 },
-    { title: 'California love!', body: 'lorem ipsum...', author: 'Guppy', id: 3 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Panda', id: 4 }
+    { title: 'I am intercontinental, cuz!', body: 'lorem ipsum...', author: 'bear', id: 1 },
+    { title: 'Welcome to the party!', body: 'lorem ipsum...', author: 'cub', id: 2 },
+    { title: 'California love!', body: 'lorem ipsum...', author: 'guppy', id: 3 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'panda', id: 4 }
   ]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'bear')} title="Bear's Blogs!" />
     </div>
   );
 }
