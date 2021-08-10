24bea4262a54626d990baebec2912c30eb13b67f
mtgrunt <mileswallace@lavascript.com>
2021-08-09 18:06:18 -0700

    Updated BlogList Home Navbar

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,19 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'I am intercontinental, cuz!', body: 'lorem ipsum...', author: 'bear', id: 1 },
-    { title: 'Welcome to the party!', body: 'lorem ipsum...', author: 'cub', id: 2 },
-    { title: 'California love!', body: 'lorem ipsum...', author: 'guppy', id: 3 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'panda', id: 4 }
-  ]);
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'bear')} title="Bear's Blogs!" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,17 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>Miles Wallace</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/blog">Blog</a>
-            </div>
-        </nav>
-     );
-}
- 
-export default Navbar;
+    return (
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#0b9106',
+            borderRadius: '8px' 
+          }}>New Blog</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
