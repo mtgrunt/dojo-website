2f2c57d1b893fdd63c3e86816fe73b3025f1b832
mtgrunt <mileswallace@lavascript.com>
2021-08-22 22:52:19 -0700

    Updated Personal

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,8 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'Bear', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Guppy', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Cub', id: 3 }
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Cub', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Guppy', id: 3 }
   ])
 
   const handleDelete = (id) => {
