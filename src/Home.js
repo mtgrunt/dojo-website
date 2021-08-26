d6a464bde12cb4942ce391d164d1d5d952dfb295
mtgrunt <mileswallace@lavascript.com>
2021-08-25 20:28:13 -0700

    Changed Name

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,9 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'Bear', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Cub', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Guppy', id: 3 }
-  ])
+  ]);
+
+  const [name, setName] = useState('Guppy');
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -15,12 +17,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use efftect ran');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Cub')}>Change Name</button>
+      <p>{ name }</p>
     </div>
   );
 }
